2a13c4dc887965732696493142448c4ddab2715e
BatiOli9 <oliversafdieh@gmail.com>
2024-08-15 15:49:56 -0300

    Creacion de Rutas para obtener todos los usuarios, usuarios especificos, crear y en proceso de terminar el de edicion de usuarios

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -10,14 +10,16 @@ const controller = {
         const apellido = req.body.apellido;
         const username = req.body.username;
         const email = req.body.email;
+        const password = req.body.password;
         const ocupacion = req.body.ocupacion;
         const pais = req.body.pais;
 
-        const jerarquia = 0;
+        const jerarquia = 1;
 
         console.log(
             nombre,
             apellido,
+            password,
             username,
             email,
             ocupacion,
@@ -25,18 +27,71 @@ const controller = {
             jerarquia
         );
 
-        let query = 'INSERT INTO public."users" (nombre, apellido, username, email, id_jerarquia, id_ocupacion, id_pais) VALUES ($1, $2, $3, $4, $5, $6, $7)';
+        let query = 'INSERT INTO public.users (nombre, apellido, username, email, id_jerarquia, id_ocupacion, id_pais, password) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)';
 
         try {
-            await client.query(query, [nombre, apellido, username, email, jerarquia, ocupacion, pais]);
+            await client.query(query, [nombre, apellido, username, email, jerarquia, ocupacion, pais, password]);
             res.json({ message: "Usuario registrado correctamente" });
         } catch (error) {
-            res.json({ message: "Error al registrar usuario" });
+            console.error('Error al registrar usuario:', error); // Imprime el error en la consola
+            res.status(500).json({ message: "Error al registrar usuario", error: error.message });
         }
     },
-    register: (req, res) => {
-        res.render("register");
+    allUsers: async (req, res) => {
+        let query = 'SELECT * FROM public.users';
+        try {
+            const result = await client.query(query);
+            res.json(result);
+        } catch (err) {
+            console.error('Error al requerir usuario:', err); // Imprime el error en la consola
+            res.status(500).json({ message: "Error al requerir usuario", err: err.message });
+        }
+    },
+    userById: async (req, res) => {
+        const id = req.params.id;
+        let query = "SELECT id, nombre, apellido, username, email, id_jerarquia, id_ocupacion, id_pais, password FROM public.users WHERE id = $1";
+    
+        try {
+            const result = await client.query(query, [id]);
+            if (result.rows.length > 0) {
+                res.json(result.rows[0]); // Devuelve solo los datos del usuario
+            } else {
+                res.status(404).json({ message: "Usuario no encontrado" });
+            }
+        } catch (err) {
+            console.error('Error al requerir usuario:', err); // Imprime el error en la consola
+            res.status(500).json({ message: "Error al requerir usuario", err: err.message });
+        }
+    },
+    editUser: async (req, res) => {
+        const id = req.params.id;
+        const { nombre, apellido, username, email, id_pais, id_ocupacion, password } = req.body;
+    
+        // Verificar que todos los campos requeridos están presentes
+        if (!nombre || !apellido || !username || !email || !id_pais || !id_ocupacion || !password) {
+            return res.status(400).json({ message: "Todos los campos son requeridos" });
+        }
+    
+        console.log(
+            nombre,
+            apellido,
+            email,
+            username,
+            password,
+            id_pais,
+            id_ocupacion
+        );
+    
+        let query = 'UPDATE public.users SET nombre = $1, apellido = $2, username = $3, password = $4, email = $5, id_pais = $6, id_ocupacion = $7 WHERE id = $8';
+    
+        try {
+            await client.query(query, [nombre, apellido, username, password, email, id_pais, id_ocupacion, id]);
+            res.send("Usuario Editado Correctamente");
+        } catch (err) {
+            console.error('Error al editar usuario:', err); // Imprime el error en la consola
+            res.status(500).json({ message: "Error al editar usuario", err: err.message });
+        }
     }
 }
 
-export default controller; 
+export default controller;
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -3,8 +3,16 @@ const router = express.Router();
 
 import userController from "../controllers/userController.js";
 
-// Rutas Registrar
-router.get("/register", userController.register);
+// Registrar Usuario
 router.post("/register", userController.registerPost);
+
+// Devolver todos los usuarios
+router.get("/allUsers", userController.allUsers);
+
+// Devolver usuario por id
+router.get("/user/:id", userController.userById);
+
+// Editar un usuario existente
+router.put("/editUser/:id", userController.editUser);
 
 export default router;
