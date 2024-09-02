c4e53873eaa850cb827013d3061192d11f0c66a1
BatiOli9 <oliversafdieh@gmail.com>
2024-09-02 14:01:42 -0300

    Creando rutas de analisis y poniendo el multer en el router

--- src/controllers/analyzeController.js
+++ src/controllers/analyzeController.js
@@ -1,25 +1,47 @@
-import multer from "multer";
-import { fileURLToPath } from "url";
-import { dirname, join, extname } from "path";
+import fs from "fs";
+import path from "path";
+import { client } from "../dbconfig.js";
+import { dirname, join, extname } from 'path';
+import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-
-const uploadDir = join(__dirname, "../uploads");
-
-const storage = multer.diskStorage({
-    destination: function (req, res, cb) {
-        cb(null, uploadDir);
-    },
-    filename: function (req, res, cb) {
-        cb(null, `${Date.now()}-${file.originalname}`)
-    }
-});
-
-const upload = multer( { storage: storage } );
+const uploadDir = path.join(__dirname, '../../uploads');
+const extensionesPermitidas = ['pdf', 'png', 'jpeg', 'jpg'];
 
 const controller = {
-    
+    todosAnalisis: async (req, res) => {
+        const query = "SELECT * FROM public.analisis";
+        try {
+            const result = await client.query(query);
+            res.json(result);
+        } catch (err) {
+            console.error('Error al requerir analisis:', err); // Imprime el error en la consola
+            res.status(500).json({ message: "Error al requerir analisis", err: err.message });
+        }
+    },
+    analisisPorPaciente: async (req, res) => {
+        const id = req.params.id;
+        const query = "SELECT * FROM public.analisis WHERE id_paciente = $1";
+        try {
+            const result = await client.query(query, [id]);
+            res.json(result);
+        } catch (err) {
+            console.error('Error al requerir analisis:', err); // Imprime el error en la consola
+            res.status(500).json({ message: "Error al requerir analisis", err: err.message });
+        }
+    },
+    analisisPorId: async (req, res) => {
+        const id = req.params.id;
+        const query = "SELECT * FROM public.analisis WHERE id = $1";
+        try {
+            const result = await client.query(query, [id]);
+            res.json(result);
+        } catch (err) {
+            console.error('Error al requerir analisis:', err); // Imprime el error en la consola
+            res.status(500).json({ message: "Error al requerir analisis", err: err.message });
+        }
+    }
 }
 
 export default controller;
--- src/routers/analyzeRouter.js
+++ src/routers/analyzeRouter.js
@@ -1,6 +1,32 @@
 import express from "express";
+import multer from 'multer';
+import { fileURLToPath } from 'url';
+import { dirname, join, extname } from 'path';
 const router = express.Router();
 
-import analyzeController from "../controllers/analyzeController.js"
+import analyzeController from "../controllers/analyzeController.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const uploadDir = join(__dirname, "../uploads");
+
+const storage = multer.diskStorage({
+    destination: function (req, res, cb) {
+        cb(null, uploadDir);
+    },
+    filename: function (req, res, cb) {
+        cb(null, `${Date.now()}-${file.originalname}`)
+    }
+});
+
+// Devolver todos los analisis
+router.get("/todosAnalisis", analyzeController.todosAnalisis);
+// Devolver analisis por paciente
+router.get("/analisisPorPaciente/:id", analyzeController.analisisPorPaciente);
+// Devolver analisis por ID
+router.get("/analisisPorId", analyzeController.analisisPorId);
+
+const upload = multer({ storage: storage });
 
 export default router;
