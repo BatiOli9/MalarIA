5c74bbd7542446cf12ca66f1bb75007c96ad3762
BatiOli9 <oliversafdieh@gmail.com>
2024-09-03 08:27:52 -0300

    subida de archivos a la base de datos con link a cloudinary

--- src/controllers/analyzeController.js
+++ src/controllers/analyzeController.js
@@ -8,7 +8,6 @@ import { upload } from "../upload.js";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const uploadDir = path.join(__dirname, '../../uploads');
-const extensionesPermitidas = ['pdf', 'png', 'jpeg', 'jpg'];
 
 const controller = {
     todosAnalisis: async (req, res) => {
@@ -47,7 +46,41 @@ const controller = {
         res.render('upload');
     },
     uploadAnalyzePost: async (req, res) => {
-        const 
+        const imageFile = req.file.filename;
+        const nombre = req.body.nombre;
+        const fecha = Date.now();
+        const id_paciente = 3;
+        
+        const extension = file_name.split('.').pop();
+        
+        const extensionesPermitidas = ['pdf', 'png', 'jpeg', 'jpg'];
+
+        if (!extensionesPermitidas.includes(extension)) {
+            console.error('Extensión de archivo no permitida');
+            return res.status(400).send('Error: Extensión de archivo no permitida. Extensiones admitidas: DOC, DOCX, XLSX, PPT, PPTX y PDF');
+        }
+
+        console.log(
+            'Nombre:', nombre,
+            'Fecha:', fecha,
+            'Id Paciente:', id_paciente,
+            'Imagen:', image
+        );
+        
+        const imageLocation = "../../uploads/" + imageFile;
+
+        console.log(imageLocation);
+
+        /* const query = 'INSERT INTO public.analisis (imagen, nombre, fecha, id_paciente) VALUES ($1, $2, $3, $4)';
+
+        try {
+            upload.uploader.upload()
+            await client.query(query, [image, nombre, fecha, id_paciente]);
+            res.json({ message: "Analisis subido correctamente" });
+        } catch (error) {
+            console.error('Error al subir analisis:', error); // Imprime el error en la consola
+            res.status(500).json({ message: "Error al subir analisis", error: error.message });
+        } */
     }
 }
 
