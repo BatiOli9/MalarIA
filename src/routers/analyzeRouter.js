9e8ae932e57c968a44060dc537c29285f76de9ab
BatiOli9 <oliversafdieh@gmail.com>
2024-09-16 08:36:50 -0300

    Solucion error carpeta en el multer

--- src/routers/analyzeRouter.js
+++ src/routers/analyzeRouter.js
@@ -13,11 +13,6 @@ const __dirname = dirname(__filename);
 
 const uploadDir = join(__dirname, "../uploads");
 
-// Verificar si la carpeta existe, si no, crearla
-if (!fs.existsSync(uploadDir)) {
-    fs.mkdirSync(uploadDir, { recursive: true });
-}
-
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, uploadDir);
