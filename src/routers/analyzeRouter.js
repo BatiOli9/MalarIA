5d59d928b8bc54ab7113837d5ef3c0ddd881ac12
BatiOli9 <oliversafdieh@gmail.com>
2024-09-24 14:26:53 -0300

    Token in Router Analyze

--- src/routers/analyzeRouter.js
+++ src/routers/analyzeRouter.js
@@ -3,6 +3,7 @@ import multer from 'multer';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 import fs from 'fs';
+import { verifyToken } from "../middlewares/auth.js";
 
 const router = express.Router();
 
